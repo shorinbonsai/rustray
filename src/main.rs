c42edf106b501ca581ed76b7956dbce6a647ddbd
James Sargant <shorinbonsai@gmail.com>
2024-04-21 18:45:58 -0400

    Vec3 fleshed out

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use std::fs::File;
 use std::io::Write;
 
 pub mod vec3;
+use crate::vec3::write_color;
+use crate::vec3::write_color_png;
+use crate::vec3::Vec3;
 
 const NX: u32 = 256;
 const NY: u32 = 256;
@@ -22,25 +25,33 @@ fn main() -> Result<(), Box<dyn Error>> {
         let tmp = NY - j;
         println!("Lines remaining: {tmp}");
         for i in 0..NX {
-            let r = i as f64 / (NX as f64 - 1.0);
-            let g = j as f64 / (NY as f64 - 1.0);
-            let b = 0.0;
-
-            let ir = (COLOR * r) as u8;
-            let ig = (COLOR * g) as u8;
-            let ib = (COLOR * b) as u8;
+            // let pixel_color = Vec3::default();
+            let pixel_color =
+                Vec3::new(i as f64 / (NX - 1) as f64, j as f64 / (NY - 1) as f64, 0.0);
+            // {
+            //     e: [i as f64 / (NX - 1) as f64, j as f64 / (NY - 1) as f64, 0.0],
+            // };
+            write_color(&mut ppm_file, &pixel_color)?;
+            write_color_png(&mut png_image, &pixel_color, i, j)?;
+            // let r = i as f64 / (NX as f64 - 1.0);
+            // let g = j as f64 / (NY as f64 - 1.0);
+            // let b = 0.0;
+            //
+            // let ir = (COLOR * r) as u8;
+            // let ig = (COLOR * g) as u8;
+            // let ib = (COLOR * b) as u8;
 
             // Write to PPM file
-            writeln!(ppm_file, "{} {} {}", ir, ig, ib)?;
+            // writeln!(ppm_file, "{} {} {}", ir, ig, ib)?;
 
             // Write to PNG image
-            png_image.put_pixel(i, j, image::Rgb([ir, ig, ib]));
+            // png_image.put_pixel(i, j, image::Rgb([ir, ig, ib]));
         }
     }
 
     println!("Done");
     // Save PNG image
-    png_image.save("out.png")?;
+    png_image.save("newpng.png")?;
 
     Ok(())
 }
--- src/vec3.rs
+++ src/vec3.rs
@@ -1,28 +1,103 @@
 use core::panic;
+use std::fmt;
+use std::fs::File;
+use std::io::Write;
 use std::ops;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
-struct Vec3 {
+pub struct Vec3 {
     e: [f64; 3],
 }
 
 impl Vec3 {
+    #[allow(dead_code)]
     pub fn new(e0: f64, e1: f64, e2: f64) -> Self {
         Self { e: [e0, e1, e2] }
     }
 
+    #[allow(dead_code)]
     pub fn x(&self) -> f64 {
         return self.e[0];
     }
+    #[allow(dead_code)]
     pub fn y(&self) -> f64 {
         return self.e[1];
     }
+    #[allow(dead_code)]
     pub fn z(&self) -> f64 {
         return self.e[2];
     }
 
+    #[allow(dead_code)]
     pub fn length_squared(&self) -> f64 {
         self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]
+    }
+
+    #[allow(dead_code)]
+    pub fn length(&self) -> f64 {
+        let tmp: f64 = self.length_squared().sqrt();
+        return tmp;
+    }
+
+    #[allow(dead_code)]
+    pub fn mul_assign(&mut self, t: f64) {
+        self.e[0] *= t;
+        self.e[1] *= t;
+        self.e[2] *= t;
+    }
+
+    #[allow(dead_code)]
+    pub fn div_assign(&mut self, t: f64) {
+        if t == 0.0 {
+            panic!("division by zero is nono");
+        }
+        self.e[0] /= t;
+        self.e[1] /= t;
+        self.e[2] /= t;
+    }
+
+    #[allow(dead_code)]
+    pub fn div(self, t: f64) -> Self {
+        if t == 0.0 {
+            panic!("division by zero is nono");
+        }
+        Self {
+            e: [self.e[0] / t, self.e[1] / t, self.e[2] / t],
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn mul(&self, scalar: f64) -> Self {
+        Self {
+            e: [self.e[0] * scalar, self.e[1] * scalar, self.e[2] * scalar],
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn dot(&self, other: &Self) -> f64 {
+        return self.e[0] * other.e[0] + self.e[1] * other.e[1] + self.e[2] * other.e[2];
+    }
+
+    #[allow(dead_code)]
+    pub fn cross(&self, other: &Self) -> Self {
+        Self {
+            e: [
+                self.e[1] * other.e[2] - self.e[2] * other.e[1],
+                self.e[2] * other.e[0] - self.e[0] * other.e[2],
+                self.e[0] * other.e[1] - self.e[1] * other.e[0],
+            ],
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn unit_vector(&self) -> Self {
+        return *self / self.length();
+    }
+}
+
+impl fmt::Display for Vec3 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {} {}", self.e[0], self.e[1], self.e[2])
     }
 }
 
@@ -41,38 +116,11 @@ impl ops::Index<usize> for Vec3 {
     }
 }
 
-// impl ops::AddAssign for Vec3 {
-//     fn add_assign(&mut self, v: Self) {
-//         *self = Self {
-//             e: [self.e[0] + v.e[0], self.e[1] + v.e[1], self.e[2] + v.e[2]],
-//         };
-//     }
-// }
-
 impl ops::AddAssign for Vec3 {
     fn add_assign(&mut self, v: Self) {
-        *self.e[0] = *self.e[0] + v.e[0];
-        *self.e[1] = *self.e[1] + v.e[1];
-        *self.e[2] = *self.e[2] + v.e[2];
-    }
-}
-
-impl ops::MulAssign for Vec3 {
-    fn mul_assign(&mut self, t: f64) {
-        *self.e[0] *= t;
-        *self.e[1] *= t;
-        *self.e[2] *= t;
-    }
-}
-
-impl ops::DivAssign<f64> for Vec3 {
-    fn div_assign(&mut self, t: f64) {
-        if t == 0.0 {
-            panic!("Division by zero is nono");
-        }
-        *self.e[0] /= t;
-        *self.e[1] /= t;
-        *self.e[2] /= t;
+        self.e[0] = self.e[0] + v.e[0];
+        self.e[1] = self.e[1] + v.e[1];
+        self.e[2] = self.e[2] + v.e[2];
     }
 }
 
@@ -90,8 +138,100 @@ impl ops::Sub for Vec3 {
     }
 }
 
+impl ops::Add for Vec3 {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        Self {
+            e: [
+                self.e[0] + other.e[0],
+                self.e[1] + other.e[1],
+                self.e[2] + other.e[2],
+            ],
+        }
+    }
+}
+
+impl ops::Mul for Vec3 {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self::Output {
+        Self {
+            e: [
+                self.e[0] * other.e[0],
+                self.e[1] * other.e[1],
+                self.e[2] * other.e[2],
+            ],
+        }
+    }
+}
+
+impl ops::Mul<f64> for Vec3 {
+    type Output = Self;
+    fn mul(self, scalar: f64) -> Self::Output {
+        Self {
+            e: [self.e[0] * scalar, self.e[1] * scalar, self.e[2] * scalar],
+        }
+    }
+}
+
+impl ops::Div<f64> for Vec3 {
+    type Output = Self;
+    fn div(self, t: f64) -> Self {
+        if t == 0.0 {
+            panic!("division by zero is nono");
+        }
+        Self {
+            e: [self.e[0] / t, self.e[1] / t, self.e[2] / t],
+        }
+    }
+}
+
 impl Default for Vec3 {
     fn default() -> Self {
         Vec3 { e: [0.0, 0.0, 0.0] }
     }
 }
+
+type Color = Vec3;
+
+pub fn write_color(file: &mut File, pixel_color: &Color) -> Result<(), std::io::Error> {
+    let r = pixel_color.e[0];
+    let g = pixel_color.e[1];
+    let b = pixel_color.e[2];
+
+    // Translate the [0,1] component values to the byte range [0,255].
+    let rbyte = (255.999 * r) as i32;
+    let gbyte = (255.999 * g) as i32;
+    let bbyte = (255.999 * b) as i32;
+
+    // Write out the pixel color components.
+    writeln!(file, "{} {} {}", rbyte, gbyte, bbyte)?;
+
+    Ok(())
+}
+
+pub fn write_color_png(
+    file: &mut image::RgbImage,
+    pixel_color: &Color,
+    x: u32,
+    y: u32,
+) -> Result<(), std::io::Error> {
+    let r = pixel_color.e[0];
+    let g = pixel_color.e[1];
+    let b = pixel_color.e[2];
+
+    // Translate the [0,1] component values to the byte range [0,255].
+    let rbyte = (255.999 * r) as i32;
+    let gbyte = (255.999 * g) as i32;
+    let bbyte = (255.999 * b) as i32;
+
+    // Write out the pixel color components.
+    file.put_pixel(x, y, image::Rgb([rbyte as u8, gbyte as u8, bbyte as u8]));
+    // writeln!(file, "{} {} {}", rbyte, gbyte, bbyte)?;
+
+    Ok(())
+}
+
+
+
